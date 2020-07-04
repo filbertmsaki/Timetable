a420c19f27df2adeb3f7333eb41f5878ff176055
filbertmsaki <msakifil111@gmail.com>
2020-07-04 11:13:28 +0300

    Update few changes

---
+++ app/src/main/java/com/example/timetable/SettingsActivity.java
@@ -0,0 +1,35 @@
+package com.example.timetable;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SettingsActivity extends AppCompatActivity {
+     Button goBackBtn;
+    @SuppressLint("RestrictedApi")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings_layout);
+        getSupportActionBar().setDefaultDisplayHomeAsUpEnabled(true);
+
+
+
+        goBackBtn = (Button) findViewById(R.id.go_back);
+
+        goBackBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SettingsActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+
+}
